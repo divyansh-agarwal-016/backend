bce8c71270d3d8d881afd5774b263297548ecb6e
divyanshagarwal <divyanshagarwal026@gmail.com>
2026-06-30 01:02:00 +0530

    signup Endpoint

--- auth/dist/index.js
+++ auth/dist/index.js
@@ -1,3 +1,8 @@
-console.log(1);
-export {};
+import { configDotenv } from "dotenv";
+import express from 'express';
+import { userRouter } from "./Routes/userRouter.js";
+const app = express();
+app.use(express.json());
+app.use('/user', userRouter);
+app.listen(3000);
 //# sourceMappingURL=index.js.map
